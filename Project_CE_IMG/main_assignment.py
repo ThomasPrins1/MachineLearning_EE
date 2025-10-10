2c2cd91c20069d43675b549384dbb023ab5d3596
ThomasPrins <thomas.prins1@hotmail.com>
2025-10-10 14:30:50 +0200

    Setup enviroment and python file

---
+++ Project_CE_IMG/main_assignment.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 16 12:59:05 2025
+
+@author: user
+@studentID: 5885221 & 
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score, accuracy_score, roc_curve, precision_recall_curve, PrecisionRecallDisplay, roc_auc_score, RocCurveDisplay, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
+from math import isclose
+np.random.seed(19680806)
+
+
+""" Variables """
+
+""" Functions """
+
+""" Main Code """
+""" Initialize Dataset """
+""" Model 1 """
+""" Model 2 """
+""" Model 3 """
+""" Model 4 """
+
+""" Plotting """
+# plt.savefig("something.png")
